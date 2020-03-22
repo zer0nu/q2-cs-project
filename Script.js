229e38b6b0ef9279f8646135b2ba9647947dca6d
zer0nu <56258662+zer0nu@users.noreply.github.com>
2020-03-22 19:38:34 +0800

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,63 +1,77 @@
-//Function for button for opening/closing side menu
-function openNav(){
-	document.getElementById("mySidebar").style.width = "250px"
-}
-document.getElementById("openbtn").addEventListener("click", openNav)
-function closeNav(){
-	document.getElementById("mySidebar").style.width = "0"
-}
-document.getElementById("closebtn").addEventListener("click", closeNav)
-//Functions that change photo being displayed
-var slideIndex = 1;
-showSlides(slideIndex)
-function currentSlide(n){
-	showSlides(slideIndex = n)
-}
-document.getElementById("one").addEventListener("click", currentSlide(1))
-document.getElementById("two").addEventListener("click", currentSlide(2))
-document.getElementById("three").addEventListener("click", currentSlide(3))
-document.getElementById("four").addEventListener("click", currentSlide(4))
-document.getElementById("five").addEventListener("click", currentSlide(5))
-function showSlides(n){
-	var i;
-	var slides = document.getElementsByClassName("mySlides")
-	var dots = document.getElementsByClassName("demo")
-	var captionText = document.getElementById("caption")
-	if (n > slides.length) {slideIndex = 1}
-	if (n < 1) {slideIndex = slides.length}
-	for (i = 0; i < slides.length; i++){
-		slides[i].style.display = "none"
-	}
-	for (i = 0; i < dots.length; i++) {
-		dots[i].className = dots[i].className.replace(" active", "")
-	}
-	slides[slideIndex-1].style.display = "block"
-	dots[slideIndex-1].className += " active"
-	captionText.innerHTML = dots[slideIndex-1].alt
-}
-//Functions that allow user to toggle between day and night mode
-function nightmode(){
-	document.body.style.backgroundColor = "#2e2d2e";
-	document.body.style.color = "white";
-	document.body.getElementsByTagName("div")[0].style.backgroundColor = "black";
-	document.body.getElementsByClassName("footer")[0].style.backgroundColor = "black";
-	document.getElementById("openbtn").style.backgroundColor = "black";
-	document.body.getElementsByClassName("sidebar")[0].style.backgroundColor = "black";
-}
-function lightmode(){
-	document.body.style.backgroundColor = "white";
-	document.body.style.color = "black";
-	document.body.getElementsByTagName("div")[0].style.backgroundColor = "lightgray";
-	document.body.getElementsByClassName("footer")[0].style.backgroundColor = "#54B948";
-	document.getElementById("openbtn").style.backgroundColor = "#54B948";
-	document.body.getElementsByClassName("sidebar")[0].style.backgroundColor = "#54B948";
-} 
-//Function that checks if the password you wrote is the same as the one in the confirm password
+//confirm password functions
 function confpw(){
     if (document.getElementById("first").value == document.getElementById("second").value){
-        alert("The passwords match.")
+		changepic()
     } 
 	else {
         alert("The passwords do NOT match.")
     }
+}
+//function that changes picture upon login
+function changepic(){
+	pic = document.getElementById("switch").value
+	localStorage.pic = pic
+	document.getElementById("user").src = pic
+}
+if (localStorage.pic != null){
+	pic = localStorage.pic
+	document.getElementById("user").src = pic
+}
+//function for theme change
+counter = 0
+function changetheme(){
+	counter++
+	if (counter == 1){
+		document.getElementsByClassName("header")[0].style.backgroundColor = "#FFCCCB"
+		document.getElementsByClassName("footer")[0].style.backgroundColor = "#FFCCCB"
+		document.getElementsByClassName("FAQsfooter")[0].style.backgroundColor = "#FFCCCB"
+		document.getElementsByClassName("OurVisionfooter")[0].style.backgroundColor = "#FFCCCB"
+		document.getElementsByClassName("conatainer")[0].style.backgroundColor = "#FFCCCB"
+		document.getElementsByClassName("FACTS")[0].style.backgroundColor = "#FFCCCB"
+		document.getElementsByClassName("SFACTS")[0].style.backgroundColor = "#FFCCCB"
+		document.getElementsByClassName("author")[0].style.backgroundColor = "#FFCCCB"
+		document.getElementsByClassName("author")[1].style.backgroundColor = "#FFCCCB"
+		document.getElementsByClassName("author")[2].style.backgroundColor = "#FFCCCB"
+		document.getElementsByClassName("author")[3].style.backgroundColor = "#FFCCCB"
+	}
+	else if (counter == 2){
+		document.getElementsByClassName("header")[0].style.backgroundColor = "#ADD8E6"
+		document.getElementsByClassName("footer")[0].style.backgroundColor = "#ADD8E6"
+		document.getElementsByClassName("FAQsfooter")[0].style.backgroundColor = "#ADD8E6"
+		document.getElementsByClassName("OurVisionfooter")[0].style.backgroundColor = "#ADD8E6"
+		document.getElementsByClassName("conatainer")[0].style.backgroundColor = "#ADD8E6"
+		document.getElementsByClassName("FACTS")[0].style.backgroundColor = "#ADD8E6"
+		document.getElementsByClassName("SFACTS")[0].style.backgroundColor = "#ADD8E6"
+		document.getElementsByClassName("author")[0].style.backgroundColor = "#ADD8E6"
+		document.getElementsByClassName("author")[1].style.backgroundColor = "#ADD8E6"
+		document.getElementsByClassName("author")[2].style.backgroundColor = "#ADD8E6"
+		document.getElementsByClassName("author")[3].style.backgroundColor = "#ADD8E6"
+	}
+	else if (counter == 3){
+		document.getElementsByClassName("header")[0].style.backgroundColor = "#90EE90"
+		document.getElementsByClassName("footer")[0].style.backgroundColor = "#90EE90"
+		document.getElementsByClassName("FAQsfooter")[0].style.backgroundColor = "#90EE90"
+		document.getElementsByClassName("OurVisionfooter")[0].style.backgroundColor = "#90EE90"
+		document.getElementsByClassName("conatainer")[0].style.backgroundColor = "#90EE90"
+		document.getElementsByClassName("FACTS")[0].style.backgroundColor = "#90EE90"
+		document.getElementsByClassName("SFACTS")[0].style.backgroundColor = "#90EE90"
+		document.getElementsByClassName("author")[0].style.backgroundColor = "#90EE90"
+		document.getElementsByClassName("author")[1].style.backgroundColor = "#90EE90"
+		document.getElementsByClassName("author")[2].style.backgroundColor = "#90EE90"
+		document.getElementsByClassName("author")[3].style.backgroundColor = "#90EE90"
+	}
+	else if (counter == 4 && counter == 0){
+		document.getElementsByClassName("header")[0].style.backgroundColor = "#FFFFFF"
+		document.getElementsByClassName("footer")[0].style.backgroundColor = "#FFFFFF"
+		document.getElementsByClassName("FAQsfooter")[0].style.backgroundColor = "#FFFFFF"
+		document.getElementsByClassName("OurVisionfooter")[0].style.backgroundColor = "#FFFFFF"
+		document.getElementsByClassName("conatainer")[0].style.backgroundColor = "#FFFFFF"
+		document.getElementsByClassName("FACTS")[0].style.backgroundColor = "#FFFFFF"
+		document.getElementsByClassName("SFACTS")[0].style.backgroundColor = "#FFFFFF"
+		document.getElementsByClassName("author")[0].style.backgroundColor = "#FFFFFF"
+		document.getElementsByClassName("author")[1].style.backgroundColor = "#FFFFFF"
+		document.getElementsByClassName("author")[2].style.backgroundColor = "#FFFFFF"
+		document.getElementsByClassName("author")[3].style.backgroundColor = "#FFFFFF"
+		counter = 0
+	}
 }
