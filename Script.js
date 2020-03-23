2c4a3fa07fb7d0d7ca475710cc5393ef704dd31f
zer0nu <56258662+zer0nu@users.noreply.github.com>
2020-03-23 15:41:06 +0800

    Update Script.js

--- Script.js
+++ Script.js
@@ -26,39 +26,18 @@ function changetheme(){
 		document.getElementsByClassName("footer")[0].style.backgroundColor = "#FFCCCB"
 		document.getElementsByClassName("FAQsfooter")[0].style.backgroundColor = "#FFCCCB"
 		document.getElementsByClassName("OurVisionfooter")[0].style.backgroundColor = "#FFCCCB"
-		document.getElementsByClassName("conatainer")[0].style.backgroundColor = "#FFCCCB"
-		document.getElementsByClassName("FACTS")[0].style.backgroundColor = "#FFCCCB"
-		document.getElementsByClassName("SFACTS")[0].style.backgroundColor = "#FFCCCB"
-		document.getElementsByClassName("author")[0].style.backgroundColor = "#FFCCCB"
-		document.getElementsByClassName("author")[1].style.backgroundColor = "#FFCCCB"
-		document.getElementsByClassName("author")[2].style.backgroundColor = "#FFCCCB"
-		document.getElementsByClassName("author")[3].style.backgroundColor = "#FFCCCB"
 	}
 	else if (counter == 2){
 		document.getElementsByClassName("header")[0].style.backgroundColor = "#ADD8E6"
 		document.getElementsByClassName("footer")[0].style.backgroundColor = "#ADD8E6"
 		document.getElementsByClassName("FAQsfooter")[0].style.backgroundColor = "#ADD8E6"
 		document.getElementsByClassName("OurVisionfooter")[0].style.backgroundColor = "#ADD8E6"
-		document.getElementsByClassName("conatainer")[0].style.backgroundColor = "#ADD8E6"
-		document.getElementsByClassName("FACTS")[0].style.backgroundColor = "#ADD8E6"
-		document.getElementsByClassName("SFACTS")[0].style.backgroundColor = "#ADD8E6"
-		document.getElementsByClassName("author")[0].style.backgroundColor = "#ADD8E6"
-		document.getElementsByClassName("author")[1].style.backgroundColor = "#ADD8E6"
-		document.getElementsByClassName("author")[2].style.backgroundColor = "#ADD8E6"
-		document.getElementsByClassName("author")[3].style.backgroundColor = "#ADD8E6"
 	}
 	else if (counter == 3){
 		document.getElementsByClassName("header")[0].style.backgroundColor = "#90EE90"
 		document.getElementsByClassName("footer")[0].style.backgroundColor = "#90EE90"
 		document.getElementsByClassName("FAQsfooter")[0].style.backgroundColor = "#90EE90"
 		document.getElementsByClassName("OurVisionfooter")[0].style.backgroundColor = "#90EE90"
-		document.getElementsByClassName("conatainer")[0].style.backgroundColor = "#90EE90"
-		document.getElementsByClassName("FACTS")[0].style.backgroundColor = "#90EE90"
-		document.getElementsByClassName("SFACTS")[0].style.backgroundColor = "#90EE90"
-		document.getElementsByClassName("author")[0].style.backgroundColor = "#90EE90"
-		document.getElementsByClassName("author")[1].style.backgroundColor = "#90EE90"
-		document.getElementsByClassName("author")[2].style.backgroundColor = "#90EE90"
-		document.getElementsByClassName("author")[3].style.backgroundColor = "#90EE90"
 	}
 	else if (counter == 4){
 		counter = 0
@@ -66,12 +45,5 @@ function changetheme(){
 		document.getElementsByClassName("footer")[0].style.backgroundColor = "#FFFFFF"
 		document.getElementsByClassName("FAQsfooter")[0].style.backgroundColor = "#FFFFFF"
 		document.getElementsByClassName("OurVisionfooter")[0].style.backgroundColor = "#FFFFFF"
-		document.getElementsByClassName("conatainer")[0].style.backgroundColor = "#FFFFFF"
-		document.getElementsByClassName("FACTS")[0].style.backgroundColor = "#FFFFFF"
-		document.getElementsByClassName("SFACTS")[0].style.backgroundColor = "#FFFFFF"
-		document.getElementsByClassName("author")[0].style.backgroundColor = "#FFFFFF"
-		document.getElementsByClassName("author")[1].style.backgroundColor = "#FFFFFF"
-		document.getElementsByClassName("author")[2].style.backgroundColor = "#FFFFFF"
-		document.getElementsByClassName("author")[3].style.backgroundColor = "#FFFFFF"
 	}
 }
